53265a371ce1a8b726a19d92ea952682ebdb1962
knakayama <knakayama.sh@gmail.com>
2016-01-17 11:27:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+antibody fast moving
+====================
+
+fast moving to antibody plugin directory with peco style
+
+# Requirements
+
+1. [antibody](https://github.com/getantibody/antibody)
+1. [peco](https://github.com/peco/peco)
+1. [zsh](http://www.zsh.org/)
+
+# Install
+
+```zsh
+antigen knakayama/antibody-fast-moving
+antigen apply
+bindkey '^x^a' -antibody-fast-moving # or your favorite key
+```
+
+# License
+
+MIT
+
+# Author
+
+[knakayama](https://github.com/knakayama)
---
+++ antibody-fast-moving.plugin.zsh
@@ -0,0 +1,34 @@
+-antibody-fast-moving() {
+  local i
+
+  for cmd in "antibody" "peco" "tmux"; do
+    if ! type "$cmd" &>/dev/null; then
+      cat <<EOT 1>&2
+$cmd does not found in your \$PATH.
+EOT
+    return 1
+    fi
+  done
+
+  if [[ -z "$ANTIBODY_HOME" ]]; then
+    cat <<'EOT' 1>&2
+ANTIBODY_HOME is not set, antibody does not work properly on your machine?
+EOT
+  return 1
+  fi
+
+  cd $(ls -1d $ANTIBODY_HOME/* | peco)
+  zle accept-line
+  zle -R -c
+}
+
+autoload -Uz -- -antibody-fast-moving
+zle -N -- -antibody-fast-moving
+
+# Local Variables:
+# mode: Shell-Script
+# sh-indentation: 2
+# indent-tabs-mode: nil
+# sh-basic-offset: 2
+# End:
+# vim: ft=zsh sw=2 ts=2 et
